e4caa5512e8c6a26d7fd96cf5507f2b25924801a
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-03-10 22:29:31 +0900

    [LIS] ing

---
+++ JLIS/JLIS/JLIS.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+//#include <set>
+
+using namespace std;
+
+int solve(vector<int> &nums1, vector<int> &nums2) {
+	/*set<int> result_set;
+	for (int i = 0; i < nums1.size(); i++) {
+		result_set.insert(nums1[i]);
+	}
+	for (int i = 0; i < nums2.size(); i++) {
+		result_set.insert(nums2[i]);
+	}
+	return result_set.size();*/
+}
+
+int main(void) {
+	int test_case, num1, num2;
+	cin >> test_case;
+
+	if (test_case < 0 || test_case>50)
+		exit(-1);
+
+	for (int i = 0; i < test_case; i++) {
+		cin >> num1;
+		cin >> num2;
+		if (num1 < 0 || num1>100)
+			exit(-1);
+		if (num2 < 0 || num2>100)
+			exit(-1);
+		vector<int> nums1(num1, 0);
+		vector<int> nums2(num2, 0);
+		for (int i = 0; i < num1; i++) {
+			cin >> nums1[i];
+		}
+		for (int i = 0; i < num2; i++) {
+			cin >> nums2[i];
+		}
+
+		cout << solve(nums1, nums2) << endl;
+	}
+	return 0;
+}
+
---
+++ LIS/LIS/LIS.cpp
@@ -0,0 +1,52 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <cstring> //memset
+
+using namespace std;
+
+int num;
+int cache[100];
+int nums[100];
+
+
+int solve_2(int start) {
+	// memoization
+	int &result = cache[start];
+	if (result == -1) {
+		result = 1;
+		for (int next = start + 1; next < num; next++) {
+			if (nums[start] < nums[next]) {
+				result = max(result, 1+solve_2(next));
+			}
+		}
+
+	}
+	return result;
+}
+
+int main(void) {
+	int test_case;
+	cin >> test_case;
+
+	if (test_case < 0 || test_case>50)
+		exit(-1);
+
+	for (int i = 0; i < test_case; i++) {
+		memset(cache, -1, sizeof(cache)); //캐시 초기화
+
+		cin >> num;
+		if (num < 0 || num>500)
+			exit(-1);
+
+		for (int i = 0; i < num; i++) {
+			cin >> nums[i];
+			if (nums[i] < 1 || nums[i]>100000)
+				exit(-1);
+		}
+
+		cout << solve_2(0) << endl;
+	}
+	return 0;
+}
+
