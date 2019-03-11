03cf69f21fc22a7c61e2a8fcd321a2b6026f141c
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-03-11 22:18:01 +0900

    [LIS] 이분 탐색

--- LIS/LIS/LIS.cpp
+++ LIS/LIS/LIS.cpp
@@ -1,12 +1,10 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include <algorithm>
-//#include <cstring> //memset
 
 using namespace std;
 
 int num;
-//int cache[101];
 int nums[500];
 int propose_nums[500];
 int lis_len_idx;
@@ -32,28 +30,6 @@ void lis4(int val) {
 	propose_nums[end_temp + 1] = val;
 }
 
-
-// 점화식 
-//int solve_2(int val) {
-//	// memoization
-//	// Check if there is a cache for next(=start+1) item.
-//	int &result = cache[start+1];
-//	if (result == -1) {
-//
-//		// Since 'start' item would always be included.
-//		result = 1;
-//
-//		// Find max len of lis for next item.
-//		for (int next = start + 1; next < num; next++) {
-//			if (start == -1 || nums[start] < nums[next]) {
-//				result = max(result, 1+solve_2(next));
-//			}
-//		}
-//
-//	}
-//	return result;
-//}
-
 int main(void) {
 	int test_case;
 	cin >> test_case;
@@ -62,9 +38,6 @@ int main(void) {
 		exit(-1);
 
 	for (int i = 0; i < test_case; i++) {
-		// Reset cache
-		//memset(cache, -1, sizeof(cache));
-
 		// reset lis_len_idx
 		lis_len_idx = 0;
 
@@ -78,7 +51,6 @@ int main(void) {
 				exit(-1);
 		}
 
-		//cout << solve_2(0) -1 << endl;
 		for (int i = 0; i < num; i++) {
 			lis4(nums[i]);
 		}
