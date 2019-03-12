2ee87adeb59d60d4cd7246c8510dfd6d30df23b1
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-03-12 21:40:26 +0900

    [JLIS] ing

--- JLIS/JLIS/JLIS.cpp
+++ JLIS/JLIS/JLIS.cpp
@@ -1,23 +1,70 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include <vector>
-//#include <set>
+#include <algorithm>
 
 using namespace std;
 
-int solve(vector<int> &nums1, vector<int> &nums2) {
-	/*set<int> result_set;
-	for (int i = 0; i < nums1.size(); i++) {
-		result_set.insert(nums1[i]);
+int num1, num2;
+int propose_nums[200];
+const long long NEGINF = numeric_limits<long long>::min();
+
+int getLisSize(vector<int> seq, int maxValue) {
+	if (seq.empty()) {
+		return 0;
 	}
-	for (int i = 0; i < nums2.size(); i++) {
-		result_set.insert(nums2[i]);
+
+	int first = seq.front();
+	seq.erase(seq.begin());
+	if (first > maxValue) {
+		return getLisSize(seq, first) + 1;
 	}
-	return result_set.size();*/
+
+	return getLisSize(seq, maxValue);
 }
 
+int getLisSize(vector<int> seq) {
+	return getLisSize(seq, NEGINF);
+}
+
+int solve(vector<int> nums1, vector<int> nums2) {
+	
+
+	vector<int> temp_nums1 = nums1;
+	vector<int> temp_nums2 = nums2;
+
+	// de-duplication
+	for (int i = 0; i < temp_nums1.size(); i++) {
+		bool is_duplicated = false;
+
+		for (int j = 0; j < num2; j++) {
+			if (temp_nums1[i] == nums2[j]) {
+				is_duplicated = true;
+				temp_nums1.erase(temp_nums1.begin() + i);
+				i--;
+				break;
+			}
+		}
+	}
+
+	// de-duplication
+	for (int i = 0; i < temp_nums2.size(); i++) {
+		bool is_duplicated = false;
+
+		for (int j = 0; j < num1; j++) {
+			if (temp_nums2[i] == nums1[j]) {
+				is_duplicated = true;
+				temp_nums2.erase(temp_nums2.begin() + i);
+				i--;
+				break;
+			}
+		}
+	}
+
+	return max(getLisSize(temp_nums1) + getLisSize(nums2), getLisSize(nums1) + getLisSize(temp_nums2));
+}
 int main(void) {
-	int test_case, num1, num2;
+	int test_case;
 	cin >> test_case;
 
 	if (test_case < 0 || test_case>50)
@@ -30,8 +77,9 @@ int main(void) {
 			exit(-1);
 		if (num2 < 0 || num2>100)
 			exit(-1);
-		vector<int> nums1(num1, 0);
-		vector<int> nums2(num2, 0);
+
+		vector<int> nums1(num1+1);
+		vector<int> nums2(num2+1);
 		for (int i = 0; i < num1; i++) {
 			cin >> nums1[i];
 		}
